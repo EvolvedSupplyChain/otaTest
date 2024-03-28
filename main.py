209c95b944ece54ddb3828706315d6ad168e85fb
EvolvedSupplyChain <161888284+EvolvedSupplyChain@users.noreply.github.com>
2024-03-28 12:12:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
 #main stuff
 import secretVars
 import json
+import time
 
 with open("config.json",'r') as f:
     config = json.load(f)
-    
+
+print("main running")
+print(config["FILEMANIFEST"])
+
 def updater():
     import otacustom
     from machine import reset
-    
-    checkList = []
     global config
+    '''
+    with open("config.json",'r') as f:
+        config = json.load(f)
+    '''
+    checkList = []
+    
+    #global config
+    updatePath = config["UPDATEPATH"]
     
     for index, files in enumerate(config['FILEMANIFEST']):
         ota_updater = otacustom.OTAUpdater(secretVars.ssid, secretVars.wifiPassword, updatePath, files['FILENAME'], files['VERSION'])
-        
-        if ota_updater.rolling_check_and_install():
-            config["FILEMANIFEST"][index]["CHECKFLAG"] = True
+        newVersion = ota_updater.rolling_check_and_install()
+        if newVersion:
+            config["FILEMANIFEST"][index]["VERSION"] = newVersion
+            config["FILEMANIFEST"][index]["CHECKFLAG"] = False
             checkList.append(True)
         else:
-            config["FILEMANIFEST"][index]["CHECKFLAG"] = False
+            config["FILEMANIFEST"][index]["CHECKFLAG"] = True
             checkList.append(False)
             
         del ota_updater
-        
-    with open("config.json",'w') as f:
-        json.dump(config, f)
     
     if all(checkList):
-        return True
+        config["BOOTMODE"] == 0
     else:
-        machine.reset()
+        config["BOOTMODE"] == 1
         
-        
-if updater():
+    with open("config.json",'w') as f:
+        json.dump(config, f)      
+    
+    return
+
+updater()
+
+time.sleep(2)
+
+if config["BOOTMODE"] == 0:
     import logger
+    #logger.main()
+elif config["BOOTMODE"] == 1:
+    machine.reset()
+else:
+    machine.reset()
         
         
     
